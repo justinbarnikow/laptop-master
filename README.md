eaac1649c0fa2c3756e18b8e72e3c6f7fb376449
Justin Barnikow <76211223+justinbarnikow@users.noreply.github.com>
2021-03-30 07:15:09 -0400

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# laptop-master
+# Laptop Customizer
+_Starter project for React course_
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+* featurecontainer
+    * feeaturelist
+        * feature item
---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.css';
+import FeatureStore from './FeatureStore'
+import FeaturesContainer from './FeaturesContainer';
+import ShoppingCart from './MainSummary/ShoppingCart';
+import Header from './Header';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selected: {
+        Processor: {
+            name: '17th Generation Intel Core HB (7 Core with donut spare)',
+            cost: 700
+          },
+        "Operating System": {
+            name: 'Ubuntu Linux 16.04',
+            cost: 200
+          },
+        "Video Card":{
+            name: 'Toyota Corolla 1.5v',
+            cost: 1150.98
+          },
+        Display: {
+            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+            cost: 1500
+          }
+      }
+    }
+  }
+
+  updateFeature(feature, newValue) {
+    const selected = Object.assign({}, this.state.selected);
+    selected[feature] = newValue;
+    this.setState({
+      selected
+    });
+  }
+
+  render() {
+    // console.log(this.state)
+    return (
+      <div className="App">
+        <Header />
+        <main>
+          <FeaturesContainer
+            features={FeatureStore}
+            handleUpdate={(feature, newValue) => this.updateFeature(feature, newValue)}
+            selected={this.state.selected}
+          />
+          <ShoppingCart
+            selected={this.state.selected}
+          />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
